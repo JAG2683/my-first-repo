b214a5e49ce2d1120050e20a964574bda2a02850
Jose Gonzalez <jag2683@gmail.com>
2024-04-09 12:46:21 -0400

    my first testing on git

---
+++ test.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class test{
+    public static void main(String[] args) {
+
+        Scanner scnr = new Scanner(System.in);
+
+        double num1 = scnr.nextDouble();
+
+        int result = (int) Math.sqrt(num1);
+
+        System.out.println("the result is " + result);
+    
+    }
+}
+
